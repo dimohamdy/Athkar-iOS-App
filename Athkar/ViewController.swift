c7ab874906fb415e0b5567115c15cca38733c87b
Ahmed Hamdy <dimo.hamdy@gmail.com>
2015-06-26 13:54:55 +0200

    Initial Commit

---
+++ Athkar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Athkar
+//
+//  Created by binaryboy on 6/26/15.
+//  Copyright (c) 2015 AhmedHamdy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
